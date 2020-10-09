28c4750123a63b86d0c7908a97303229b84704be
aabccd021 <aabccd021@gmail.com>
2020-10-09 17:49:59 +0700

    rename folder to example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -4,7 +4,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flamestore_example/docs.dart';
-import 'package:flamestore_example/flamestore/flamestore.dart';
+import 'package:flamestore/flamestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:google_sign_in/google_sign_in.dart';
