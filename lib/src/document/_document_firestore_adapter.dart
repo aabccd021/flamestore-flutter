b6e0b058a0e172de0d26fe37e19315be13e7fcfc
aabccd021 <aabccd021@gmail.com>
2020-10-30 14:07:10 +0700

    v0.0.5

--- lib/src/document/_document_firestore_adapter.dart
+++ lib/src/document/_document_firestore_adapter.dart
@@ -8,7 +8,16 @@ class _DocumentFirestoreAdapter {
 
   Future<T> get<T extends Document>(T document) async {
     final reference = document.reference;
-    final snapshot = await reference.get();
+    DocumentSnapshot snapshot;
+    try {
+      snapshot = await reference.get();
+    } on FirebaseException catch (e) {
+      if (e.code != 'permission-denied') {
+        throw (e);
+      }
+      print(e);
+      return null;
+    }
     final data = snapshot?.data();
     print('GET $reference $data');
     if (data == null) {
